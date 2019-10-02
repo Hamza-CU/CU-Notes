05682d75780e5e202da8db8e2f3761d734601232
Hamza-CU <56089995+Hamza-CU@users.noreply.github.com>
2019-10-02 14:26:48 -0400

    Delete PP1.py

--- PP1.py
+++
@@ -1,6 +0,0 @@
-#Problem 3 (Basic Input/Output and Calculation - *)
-
-name = input("Hello, whats your name (don't worry I wont tell anyone) \n")
-year = int(input("What year were you born \n"))
-age = str(2019 - year)
-print("Hello" + name + ".Your age is "+age)
