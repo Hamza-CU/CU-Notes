31b99d719dc0ffae34fa849c67765ffced376334
Hamza-CU <56089995+Hamza-CU@users.noreply.github.com>
2019-10-28 15:00:19 -0400

    Update pp14.py

--- practiceproblems/pp14.py
+++ practiceproblems/pp14.py
@@ -1,6 +1,5 @@
 #Problem 5 (Tracking Numbers - **)
 
-i = 0
 totSum = 0
 totNumCount = 0
 posNumCount = 0
@@ -9,21 +8,26 @@ average = 0
 largest = 0
 smallest = 0
 
-while i == 0:
-    response = int(input("Enter a number: "))
-    if response == int:
-        int(response)
-        totSum += response
-        totNumCount += 1
-        average = (totSum/totNumCount)
-        if response > 0:
-            posNumCount += 1
-        elif response < 0:
-            negNumCount += 1
-    else:
-        print("Positive Numbers:", posNumCount)
-        print("Negative Numbers:", negNumCount)
-        print("Average of Numbers:", average)
-        print("Largest Number:", )
-        print("Smallest Number:", )
-        break
+i = 0
+
+f = open('numbertest1.txt', 'r')
+
+number = int(f.readline().strip())
+
+while number != '':
+
+    print(number)
+    number = int(f.readline().strip())
+    int(number)
+    totSum = totSum + number
+    totNumCount = totNumCount + 1
+    average = (totSum/totNumCount)
+    if number > 0:
+        posNumCount += 1
+    elif number < 0:
+        negNumCount += 1
+
+print("Positive Numbers:", posNumCount)
+print("Negative Numbers:", negNumCount)
+print("Largest Number:", )
+print("Smallest Number:", )
